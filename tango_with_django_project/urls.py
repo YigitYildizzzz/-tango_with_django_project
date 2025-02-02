cce3827ae6434eed282b8614dcf6f87896614210
Yigit Yildiz <2954056y@student.gla.ac.uk>
2025-02-02 16:02:13 +0000

    Chapter 3 completed - URL mappings and views added

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rango import views  # Rango uygulamasını içe aktar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # Ana sayfayı Rango'ya yönlendiriyoruz
+    path('rango/', include('rango.urls')),  # Rango uygulaması için yönlendirme
 ]
+
+
