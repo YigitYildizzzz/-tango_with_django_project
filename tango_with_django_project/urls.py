622df587c75ae44975c4eee2e5ab834e09d45636
Yigit Yildiz <2954056y@student.gla.ac.uk>
2025-02-02 18:27:24 +0000

    chapter 4 exercise done

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls')),
+    path('', include('rango.urls')),
 ]
 
 if settings.DEBUG:
